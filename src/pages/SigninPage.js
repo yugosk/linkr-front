6dff13085b028b9c359d1e2ee7f73422c2a10e76
Mary Karakida <makarakida@gmail.com>
2022-08-09 14:44:14 -0300

    feat: log in to app

--- src/pages/SigninPage.js
+++ src/pages/SigninPage.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 import PageView from "../components/Authentication/PageView";
 import Description from "../components/Authentication/Description";
@@ -8,8 +9,28 @@ import Input from "../components/Authentication/Form/Input";
 import Button from "../components/Authentication/Form/Button";
 
 export default function SigninPage() {
+  const navigate = useNavigate();
+
+  const [postLoading, setPostLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   async function signIn(e) {
     e.preventDefault();
+    try {
+      setPostLoading(true);
+
+      const body = { email, password };
+      await axios.post(`${process.env.REACT_APP_API_BASE_URL}/signin`, body);
+
+      navigate("/timeline", { replace: true });
+    } catch (err) {
+      if (err.response.status === 401) {
+        alert("Invalid email or password");
+      }
+
+      setPostLoading(false);
+    }
   }
 
   return (
@@ -21,9 +42,25 @@ export default function SigninPage() {
         </div>
       </Description>
       <Form onSubmit={signIn}>
-        <Input type="email" placeholder="e-mail" required />
-        <Input type="password" placeholder="password" required />
-        <Button type="submit">Log in</Button>
+        <Input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          disabled={postLoading}
+          placeholder="e-mail"
+          required
+        />
+        <Input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          disabled={postLoading}
+          placeholder="password"
+          required
+        />
+        <Button type="submit" disabled={postLoading}>
+          Log in
+        </Button>
         <Link to="/sign-up">First time? Create an account!</Link>
       </Form>
     </PageView>
